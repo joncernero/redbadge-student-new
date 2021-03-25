feced7a0d8c8c3402fd2802793e4cc09584f8784
Jonathan Cernero <cernero.jon@gmail.com>
2021-03-25 19:32:23 -0400

    creating class counter component

--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,25 @@
 // Create a class version of the Functional Component
+import React, { Component } from 'react';
+import { Button, Container } from 'reactstrap';
+
+class ClickCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { countValue: this.props.initialValue };
+  }
+
+  handleClick = () => {
+    this.setState({ countValue: this.state.countValue - 1 });
+  };
+
+  render() {
+    return (
+      <Container>
+        <p>The total clicks left are {this.state.countValue}</p>
+        <Button onClick={this.handleClick}>Click Me From Function</Button>
+      </Container>
+    );
+  }
+}
+
+export default ClickCounter;
