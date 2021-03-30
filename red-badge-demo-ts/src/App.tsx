74ba0c6dd6cba8b1eec330dab7d61ca1725fffce
Jonathan Cernero <cernero.jon@gmail.com>
2021-03-30 17:40:52 -0400

    adding components

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainPage from './components/Main';
 
 function App() {
   return (
-    <div className="container">
-      <p>Hi from App.tsx</p>
+    <div className='container'>
+      <Header />
+      <MainPage />
+      <Footer />
     </div>
   );
 }
---
+++ red-badge-demo-ts/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export interface FooterProps {}
+
+const Footer: React.SFC<FooterProps> = () => {
+  return <div>Let them Roll</div>;
+};
+
+export default Footer;
